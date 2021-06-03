4c409e55903e1295a373836e5cf6fff20824b72a
thebao1605 <85266096+thebao1605@users.noreply.github.com>
2021-06-03 09:10:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Thẻo Hâm eii!',
+    introTitle: 'Thẻo Hâmm eii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
